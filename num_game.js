ac5f5408d2e2b4914cb7d7c4c1c72c6779e2a8ff
Namachivayam2001 <namachivayam10419u25030@gmail.com>
2024-01-04 18:15:24 +0530

    First commit

---
+++ num_game.js
@@ -0,0 +1,33 @@
+const num = document.getElementById("num");
+const button = document.getElementById("button");
+const error = document.getElementById("error");
+const score = document.getElementById("score");
+const count = document.getElementById("count");
+
+button.addEventListener("click", () => {
+    checkNumber();
+});
+
+var counter = 0;
+
+const checkNumber = () => { 
+    const rand = Math.floor(Math.random() * 10);
+    console.log(rand);
+    if(num.value < 1 || num.value > 10){
+        error.innerHTML = "num be 1 to 10";
+        error.style = "color: red; margin-left: -90px";
+        score.style = "margin-top: 2px";
+        count.innerHTML = counter;
+    }else{
+        score.style = "margin-top: 20px";
+        if(rand == num.value){
+            counter++;
+        }else{
+            counter--;
+        }
+        error.innerHTML = "rand: " + rand;
+        error.style = "color: green; margin-left: -108px; font-style: italic";
+        score.style = "margin-top: 2px";
+        count.innerHTML = counter;
+    }
+}
